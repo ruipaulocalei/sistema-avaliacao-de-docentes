0f9bdfb876a77af53d60729e52e7736dbf1f059c
ruipaulocalei <ruipaulocalei0@gmail.com>
2022-04-07 13:48:53 +0100

    App blade layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SGFE</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <main class="mx-auto container max-w-screen-md">
+        @yield('content')
+    </main>
+</body>
+
+</html>
