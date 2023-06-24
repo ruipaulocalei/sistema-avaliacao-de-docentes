6c27cea04003d6f07919525eabd89c96065fd7a9
ruipaulocalei <ruipaulocalei0@gmail.com>
2023-06-24 17:00:36 +0100

    Delete Departamento

--- app/Http/Livewire/Departamentos/MostrarDepartamentos.php
+++ app/Http/Livewire/Departamentos/MostrarDepartamentos.php
@@ -3,17 +3,33 @@
 namespace App\Http\Livewire\Departamentos;
 
 use App\Models\Departamento;
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class MostrarDepartamentos extends Component
 {
     use WithPagination;
+    public $listeners = ['eliminarDepartamento'];
     public function render()
     {
         $departamentos = Departamento::paginate(10);
         return view('livewire.departamentos.mostrar-departamentos', [
             'departamentos' => $departamentos
         ]);
+    }
+
+    public function eliminarDepartamento(Departamento $departamento)
+    {
+        try {
+            DB::beginTransaction();
+            $departamento = Departamento::lockForUpdate()->find($departamento->id);
+            $departamento->delete();
+            DB::commit();
+        } catch (\Exception $e) {
+            //$e;
+            DB::rollBack();
+        }
     }
 }
